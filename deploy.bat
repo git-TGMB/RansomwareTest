fa5adcdaa0590c11d8bb28dae96fcc69cbae6ecc
git-TGMB <133995763+git-TGMB@users.noreply.github.com>
2024-03-12 15:11:10 -0500

    Update deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -13,18 +13,23 @@ curl -L -o %Script1% %url1%
 curl -L -o %Script2% %url2%
 curl -L -o %wallpaperpath% %wallpaperurl%
 
+:: being kind and saving old wallpaper location
+for /f "tokens=3" %%a in ('reg query "HKCU\Control Panel\Desktop"  /V WallPaper ^|findstr /ri "REG_SZ"') do echo %%a > C:\temp\oldwallpaper.txt
+
 :: Hide taskbar
 powershell -command "&{$p='HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StuckRects3';$v=(Get-ItemProperty -Path $p).Settings;$v[8]=3;&Set-ItemProperty -Path $p -Name Settings -Value $v;}"
 
 :: Change wallpaper and hide icons, update
 reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d %wallpaperpath% /f
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v HideIcons /t REG_DWORD /d 1 /f
-RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters 1, True
 timeout 3
 
 :: schtask for popup.bat
 schtasks /Create /TN "%TaskName%" /TR "%Script2%" /SC MINUTE /MO 1 /RL LIMITED /F
 schtasks /Run /TN %TaskName%
+timeout 2
 
 taskkill /f /im explorer.exe
 start explorer.exe
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters 1, True
