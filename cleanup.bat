dd9b3386fc8caff987d22eb7ffa165ded2b24659
git-TGMB <133995763+git-TGMB@users.noreply.github.com>
2024-03-12 14:13:11 -0500

    Update cleanup.bat

--- cleanup.bat
+++ cleanup.bat
@@ -7,16 +7,14 @@ set "Script2=C:\temp\hide.vbs"
 powershell -command "&{$p='HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StuckRects3';$v=(Get-ItemProperty -Path $p).Settings;$v[8]=2;&Set-ItemProperty -Path $p -Name Settings -Value $v;}"
 pause
 
-:: Change wallpaper and hide icons, update
+:: Change wallpaper and show icons, update
 reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d %wallpaperpath% /f
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v HideIcons /t REG_DWORD /d 0 /f
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
-pause
 
 :: schtask for popup.bat
 schtasks /Delete /TN "%TaskName%" /F
-pause
-taskkill /f /im cmd.exe
-pause
+
 taskkill /f /im explorer.exe
 start explorer.exe
+pause
